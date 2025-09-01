b587bec39557e1205dd41706dc980b78831fa70d
Sophie Sianen <92133110+ssianen@users.noreply.github.com>
2025-09-01 12:05:23 -0700

    Updated config.h with current version

--- config.h
+++ config.h
@@ -17,25 +17,3 @@
  */
 #pragma once
 
-// Rotary Encoder Support
-// #define ENCODER_A_PINS { GP26 } //remove if unnecessary
-// #define ENCODER_B_PINS { GP28 } //remove if unnecessary
-#define ENCODER_RESOLUTION 5
-#define ENCODER_MAP_KEY_DELAY 1
-#define ENCODER_DIRECTION_FLIP
-
-// OLED Setup
-#define I2C_DRIVER I2CD0    // This is our i2c instance ()
-#define I2C_SCL_PIN GP1
-#define I2C_SDA_PIN GP0
-// #define OLED_DISPLAY_ADDRESS 0x3C
-#define OLED_DISPLAY_ADDRESS 0x3D
-#define OLED_DISPLAY_128X32
-
-// RGB Setup
-#define RGBLIGHT_LED_COUNT 16
-#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
-#define RGBLIGHT_LIMIT_VAL 255
-#define RGBLIGHT_SLEEP
-#define RGBLIGHT_ANIMATIONS
-#define WS2812_REVERSE
